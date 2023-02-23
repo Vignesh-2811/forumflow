84395af161f0f05bee4604ebe75f318ea68fc859
Vignesh-2811 <shakthivignesh2002@gmail.com>
2023-02-23 21:52:58 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Reset } from "styled-reset";
+import styled, {createGlobalStyle} from "styled-components";
+import Header from "./Header";
+
+const GlobalStyles = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300,400,700&family=Rubik&display=swap');
+body {
+  background: #2d2d2d;
+  color: #fff;
+  font-family: Roboto, sans-serif;
+}
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Reset />
+      <GlobalStyles />
+      <Header />
     </div>
   );
 }
